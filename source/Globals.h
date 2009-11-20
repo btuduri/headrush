dbf59fb61b960a1cb11e8617ba6d8e7186b827d5
retrobytesportal <retrobytesportal@5ccc8140-d5e6-11de-96bf-fbce4fd84a6e>
2009-11-20 19:49:18 +0000

    Hmmm.. Bit of play on jump mechanics and some commenting

--- source/Globals.h
+++ source/Globals.h
@@ -3,7 +3,7 @@
 
 // Having this struct inside here means it won't make a copy each time you #include it
 
-struct Ball
+struct Ball							// define the elements that construct our 'balls'
 {
 	float X;
 	float Y;
@@ -15,18 +15,21 @@ struct Ball
 };
 
 	// Defines for the physic effects
-#define 	ACCEL				0.050
-#define 	FRICTION			0.045
-#define		MAXACCEL			5
-#define		BOUNCE_X_DEADEN		1.0125
-#define		JUMPSPEED			5
-#define		GRAVITY				0.2
+#define 	ACCEL				0.050			// Horizontal acceleration on forced movement
+#define 	FRICTION			0.045			// Speed of slowdown when no force applied
+#define		MAXACCEL			5				// Maximum horizontal speed
+#define		BOUNCE_X_DEADEN		1.0125			// how much to deaden a horzontal bounce
+#define		JUMPSPEED			5				// initial speed of a jump (negetive)
+#define		GRAVITY				0.2				// force of gravity
+#define		BOUNCEFACTOR		1.75			// used to reverse a vertical drop for a bounce
+#define		BOUNCEFACTORAMOUNT	1.55			// the amount of bounce to allow on a vertical drop
 
 	// Defines for player (ball) status
 	
 #define		NORMAL				0
 #define		JUMPING				1
 #define		FALLING				2
+#define		GROUNDTOUCH			3
 
 	//	Defines for the balls type
 	
