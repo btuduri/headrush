83262f4f4cc47e9e16fdb151d2991a1315793dad
retrobytesportal <retrobytesportal@5ccc8140-d5e6-11de-96bf-fbce4fd84a6e>
2009-12-01 19:54:16 +0000

    Tweaks to movement... Trying to add a bit more control

--- source/Main.cpp
+++ source/Main.cpp
@@ -75,7 +75,7 @@ int main(void)
 	for(int i=1; i<BALLCOUNT; i++)
 	{
 		g_spriteArray[i].Action = ACTION_NONE;	
-		g_spriteArray[i].X = (rand() % (LEVEL_WIDTH-(BALLSIZE * 2))) + BALLSIZE * 2;
+		g_spriteArray[i].X = 240; //(rand() % (LEVEL_WIDTH-(BALLSIZE * 2))) + BALLSIZE * 2;
 		g_spriteArray[i].Y = 32; //(rand() % (LEVEL_HEIGHT-(BALLSIZE * 2))) + BALLSIZE;
 		g_spriteArray[i].Type = BALLTYPE_EVILBALL;
 		g_spriteArray[i].Gfx = oamAllocateGfx(&oamSub, SpriteSize_32x32, SpriteColorFormat_256Color); // allocate for each ball	
