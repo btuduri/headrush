3a48a82cc370c92f4373e46a7123b5a5a672999e
headkaze <headkaze@5ccc8140-d5e6-11de-96bf-fbce4fd84a6e>
2009-12-09 17:00:56 +0000

    Added Box2D support... phew!

--- trunk/source/Globals.cpp
+++ trunk/source/Globals.cpp
@@ -20,3 +20,12 @@ bool g_reJump = 0;
 
 Sprite g_colSprite1;
 Sprite g_colSprite2;
+
+b2AABB* g_worldAABB;
+b2World* g_world;
+b2Vec2* g_gravity;
+b2BoxDef* g_groundBoxDef;
+b2BodyDef* g_groundBodyDef;
+b2BoxDef* g_boxDef;
+b2BodyDef* g_bodyDef;
+b2Body* g_body;
