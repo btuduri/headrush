139bfbca045db41427e1d80e1c963bc629be0992
headkaze <headkaze@5ccc8140-d5e6-11de-96bf-fbce4fd84a6e>
2009-12-11 19:12:36 +0000

    Some basic movement

--- trunk/source/Globals.h
+++ trunk/source/Globals.h
@@ -107,6 +107,9 @@ struct Sprite							// define the elements that construct our 'balls'
 
 #define		SCALE				0.1F
 
+#define		WALKVEL				5.0F
+#define		JUMPVEL				1.0F
+
 #endif
 
 // This means that g_playerBall; is defined externally (in Globals.cpp)
